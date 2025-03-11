16d7dc22072ed8820bc08fca90187b39f528bf17
Bobby-Crew <bobbydcrew@gmail.com>
2025-03-11 16:05:38 +0000

    init

--- cinema_backend/settings.py
+++ cinema_backend/settings.py
@@ -89,3 +89,10 @@ AUTH_USER_MODEL = 'users.User'  # Add this line
 # Security settings
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
